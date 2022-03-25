95be9929a53b787a01bb39f330ee262a4807d0a4
Christina Conley <clconley22@gmail.com>
2022-03-25 12:08:16 -0500

    installed native elements, added starter cards

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import EventCards from './EventCards'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>YESSSS Got this shit running!</Text>
-      <Text>next step: plan app design</Text>
+      <EventCards/>
       <StatusBar style="auto" />
     </View>
   );
