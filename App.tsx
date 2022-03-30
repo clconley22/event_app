8b811f1ae005ff533001b2a1ed5c5a8c4de89881
Christina Conley <clconley22@gmail.com>
2022-03-30 18:39:18 -0500

    added dropdown package

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import EventCards from './EventCards'
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+  'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
+  'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+});
+
 
 const  App = () => {
-
-
-  return (
-    <View style={styles.container}>
-      <EventCards/>
-    </View>
-    
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+  if(fontsLoaded){
+    return (
+      <View style={styles.container}>
+        <EventCards/>
+      </View>
+      
+    );
+  } else {
+    return (
+    <AppLoading
+      startAsync={getFonts}
+      onFinish={()=>setFontsLoaded(true) }
+      onError={()=> console.log('error')}
+      />
+    )
+  }
+  
 };
 
 const styles = StyleSheet.create({
--- EventCards.tsx
+++ EventCards.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, ScrollView, StyleSheet, Image } from 'react-native';
 import { Text, Card, Button, Icon } from 'react-native-elements';
-
-
+import { SelectCountry as StatusDropdown } from 'react-native-element-dropdown';
+import * as TNB from './assets/tnb.jpeg'
 type CardsComponentsProps = {};
 type CardsContentProps = {
   eventName: string,
@@ -14,12 +14,14 @@ type CardsContentProps = {
 
 const cardContents = [
   {
+    id:1,
     eventName: 'Turnt n Burnt',
     eventDate: 'FRI, JUN 17 - SUN, JUN 19',
     eventImgSrc: 'https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg',
     eventAttendenceStatus: 'Going'
   },
   {
+    id:2,
     eventName: '4th of July',
     eventDate: 'SAT JUL 2',
     defaultColor: '#FE5F55',
@@ -27,6 +29,7 @@ const cardContents = [
     eventAttendenceStatus: 'Going'
   },
   {
+    id: 3,
     eventName: "Con and Joe's Birthday",
     eventDate: 'SAT AUG 13',
     defaultColor: '#4f6367',
@@ -35,13 +38,30 @@ const cardContents = [
   }
 ]
 
-const EventCards: React.FunctionComponent<CardsComponentsProps> = () => {
+const options = [{
+  value: '1',
+  lable: 'Going'
+},
+{
+  value: '2',
+  lable: 'Maybe'
+},
+{
+  value: '3',
+  lable: 'Not Going'
+}]
+
+
+const EventCards: React.FunctionComponent<CardsComponentsProps> = (props) => {
+
+  const [response, setResponse] = useState('Respond');
+
 return (
   <>
     <ScrollView>
       <View style={styles.container}>
-      {cardContents.map((content: CardsContentProps)=> (
-        <Card containerStyle = {styles.card}>
+      {cardContents.map((content: CardsContentProps, index: number)=> (
+        <Card key={index} containerStyle = {styles.card}>
           { content.eventImgSrc 
             ? 
             (<Card.Image
@@ -64,28 +84,28 @@ return (
               borderTopRightRadius: 25
             }}/>)
           }
-          <Text style={{ margin: 3 }}>
+          <Text style={styles.text}>
             {content.eventDate}
           </Text>
-          <Text style={{ margin: 3 }}>
+          <Text style={styles.text}>
             {content.eventName}
           </Text>
           <View style={{width:'50%', padding:5, display: 'flex', justifyContent: 'flex-end' }} >
-          <Button
-            // icon={
-            //   <Icon
-            //     name="code"
-            //     color="#ffffff"
-            //     iconStyle={{ marginRight: 10 }}
-            //   />
-            // }
-            buttonStyle={{
-              borderRadius: 10,
-              marginLeft: 0,
-              marginRight: 0,
-              marginBottom: 0,
+          <StatusDropdown
+            style={styles.dropdown}
+            selectedTextStyle={styles.selectedTextStyle}
+            placeholderStyle={styles.placeholderStyle}
+            iconStyle={styles.iconStyle}
+            maxHeight={130}
+            value={response}
+            data={options}
+            valueField="value"
+            labelField="lable"
+            imageField=""
+            placeholder="Response"
+            onChange={e => {
+              setResponse(e.value);
             }}
-            title="Going"
           />
           </View>
         </Card>
@@ -130,6 +150,31 @@ defaultColorDiv: {
 card: {
 borderRadius: 25,
 padding: 0
+},
+text: {
+  fontFamily: 'poppins-regular',
+  marginLeft: 10,
+  margin: 3,
+  fontSize: 16
+},
+dropdown: {
+  margin: 10,
+  height: 50,
+  borderColor: 'gray',
+  borderWidth: 0.5,
+  borderRadius: 8,
+},
+placeholderStyle: {
+  fontSize: 16,
+},
+selectedTextStyle: {
+  fontSize: 16,
+  marginLeft: 8,
+  fontFamily:'poppins-regular'
+},
+iconStyle: {
+  width: 20,
+  height: 20
 }
 });
 
