9529c9c5315ca7ffb75c98e7391523c75c7ed665
Christina Conley <clconley22@gmail.com>
2022-03-30 21:30:29 -0500

    update imports

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react'
 import { StyleSheet, Text, View, Platform } from 'react-native';
-import Tabs from './Tabs'
+import Tabs from './components/Tabs'
 import * as Font from 'expo-font'
 import AppLoading from 'expo-app-loading';
 
